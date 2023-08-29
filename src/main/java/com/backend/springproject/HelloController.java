445c3122cacb4a02dfd9dcb248c574529f0d6e8f
Hector <hxtr227@gmail.com>
2023-08-29 21:33:43 +0200

    First commit

---
+++ src/main/java/com/backend/springproject/HelloController.java
@@ -0,0 +1,14 @@
+package com.backend.springproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot F5!";
+    }
+
+}
